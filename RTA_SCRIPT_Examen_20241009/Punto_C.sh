60d272adff06e27ed090c591928f3b0a91361a7f
eliascharadia <eliascharadia01@gmail.com>
2024-10-11 00:43:45 +0000

    Modificacion: Agregue los cambios de persmisos y dueños en el script del punto c

--- RTA_SCRIPT_Examen_20241009/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241009/Punto_C.sh
@@ -21,3 +21,38 @@ do
 	id $USUARIO
 done
 IFS=$ANT_IFS
+
+
+echo "Cambiando permisos y propietarios"
+
+for i in {1..3}
+do
+	sudo chown -R p1c2_2024_A$i:p1c2_2024_A$i /Examenes-UTN/alumno_$i
+done
+
+sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
+
+sudo chmod -R 775 /Examenes-UTN/profesores
+sudo chmod -R 750 /Examenes-UTN/alumno_1
+sudo chmod -R 760 /Examenes-UTN/alumno_2
+sudo chmod -R 700 /Examenes-UTN/alumno_3 
+
+for i in {1..3}
+do
+	su -c "whoami > /Examenes-UTN/alumno$i/validar.txt"  p1c2_2024_A$i
+	echo
+	echo "Permisos: "
+	sudo ls -l /Examenes-UTN/alumno$i
+	echo
+	echo "Usuario: "
+	sudo cat /Examenes-UTN/alumno$i/validar.txt
+done
+
+su -c "whoami > /Examenes-UTN/profesores/validar.txt"  p1c2_2024_P1
+echo
+echo "Permisos: "
+sudo ls -l /Examenes-UTN/profesores
+echo
+echo "Usuario: "
+sudo cat /Examenes-UTN/profesores/validar.txt
+
