56fbb9d7259379b55a1302efc6799f9f933ef8d7
eliascharadia <eliascharadia01@gmail.com>
2024-10-12 15:16:43 +0000

    feat: Cambie el directorio donde direcciono la salida del whoami

--- RTA_SCRIPT_Examen_20241009/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241009/Punto_E.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Ecribo info de la memoria"
 echo
-grep -i memtotal /proc/meminfo > repogit/UTNFRA_SO_1P2C_2024_Charadia/RTA_ARCHIVOS_Examen_20241009/Filtro_Basico.txt
+grep -i memtotal /proc/meminfo > $HOME/repogit/UTNFRA_SO_1P2C_2024_Charadia/RTA_ARCHIVOS_Examen_20241009/Filtro_Basico.txt
 
 
 echo "Agrego info del chassis"
 echo
-sudo dmidecode -t chassis |grep -Ei "chassis|manufactu" >> repogit/UTNFRA_SO_1P2C_2024_Charadia/RTA_ARCHIVOS_Examen_20241009/Filtro_Basico.txt
+sudo dmidecode -t chassis |grep -Ei "chassis|manufactu" >> $HOME/repogit/UTNFRA_SO_1P2C_2024_Charadia/RTA_ARCHIVOS_Examen_20241009/Filtro_Basico.txt
